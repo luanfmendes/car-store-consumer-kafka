dbdb001f7f11add60e0c69f11326d6fa9e4e7187
luanfmendes <lna_mendes@hotmail.com>
2023-05-29 11:13:06 -0300

    update log

--- src/main/java/com/store/car/message/KafkaConsumerMessage.java
+++ src/main/java/com/store/car/message/KafkaConsumerMessage.java
@@ -19,7 +19,7 @@ public class KafkaConsumerMessage {
 
     @KafkaListener(topics = "car-post-topic", groupId = "store-posts-group")
     public void listening(CarPostDTO carPost){
-        LOG.info("Received Car Post information: {}", carPost);
+        LOG.info("CAR STORE - Received Car Post information: {}", carPost);
         carPostService.newPostDetail(carPost);
     }
 }
